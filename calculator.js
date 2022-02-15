9c807c369c955ed63d7a23f210dcc6327253b597
0-2HERO <64213996+0-2HERO@users.noreply.github.com>
2022-02-15 18:17:14 +0100

    var =>let

--- calculator.js
+++ calculator.js
@@ -1,13 +1,13 @@
 
 
-var starter = 10.95;
-var main = 15.50;
-var dessert = 12.95;
-var beverage = 10.95;
+let starter = 10.95;
+let main = 15.50;
+let dessert = 12.95;
+let beverage = 10.95;
 
-const calculateInvoice = (starterPrice, maindishPrice, dessertPrice, beveragePrice) => {
+const calculateInvoice = (starterPrice, mainDishPrice, dessertPrice, beveragePrice) => {
 
-    return (starterPrice + maindishPrice + dessertPrice + beveragePrice)
+    return (starterPrice + mainDishPrice + dessertPrice + beveragePrice)
 }
 console.log(calculateInvoice(starter, main, dessert, beverage));
 
@@ -15,28 +15,28 @@ console.log(calculateInvoice(starter, main, dessert, beverage));
 
 
 
-var poriyal = 12.95;
-var salmonVeggies = 15.50;
-var sunnyLemon = 12.95;
-var guinness = 10.95;
+let poriyal = 12.95;
+let salmonVeggies = 15.50;
+let sunnyLemon = 12.95;
+let guinness = 10.95;
 console.log(calculateInvoice(poriyal, salmonVeggies, sunnyLemon, guinness));
 
 
-var suppe = 2;
-var kebab = 11;
-var brownies = 1;
-var cola = 1.5;
+let suppe = 2;
+let kebab = 11;
+let brownies = 1;
+let cola = 1.5;
 console.log(calculateInvoice(suppe, kebab, brownies, cola));
 
-var smkdSalmon = 2.5;
-var teriyaki = 14;
-var icecream = 2;
-var iMartini = 2;
+let smkdSalmon = 2.5;
+let teriyaki = 14;
+let icecream = 2;
+let iMartini = 2;
 console.log(calculateInvoice(smkdSalmon, teriyaki, icecream, iMartini));
 
 
 const sdtInvoice = (starterPrice, maindishPrice, dessertPrice, beveragePrice) => {
-    var std = (starterPrice + maindishPrice + dessertPrice) - ((starterPrice + maindishPrice + dessertPrice) / 10)
+    let std = (starterPrice + maindishPrice + dessertPrice) - ((starterPrice + maindishPrice + dessertPrice) / 10)
 
     return (std + beveragePrice)
 }
